a76842fe90a65f69816864b8ed2299337af5f742
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-05-22 17:40:50 +0100

    Update creategamenight.js

--- ibotv1-master/commands/creategamenight.js
+++ ibotv1-master/commands/creategamenight.js
@@ -7,10 +7,10 @@ exports.run = async (Discord, client, message, args) => {
     if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Sorry, but you do not have valid permissions! If you beleive this is a error, contact an owner.");
     
 
-    let mutetime = args[1];
+    let mutetime = args[0];
     if (!mutetime) return message.reply("You didn't specify a time! Please specify how many ms from now you would like the gamenight!");
 
-    await (staffc.send("A gamenight has been scheduled!\n Game:" + game + "\n Time From Now:" + ms(mutetime)));
+    await (staffc.send("A gamenight has been scheduled!\n Game:\n Time From Now:" + ms(mutetime)));
     message.reply("Gamenight has been created for" + ms(mutetime) + "from now!");
     let announcetwo = mutetime-600000
     setTimeout(function() {
