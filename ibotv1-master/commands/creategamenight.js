84a85ca37a692e405774a6983bb9f3df1aff1ae7
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-05-22 18:16:07 +0100

    Update creategamenight.js

--- ibotv1-master/commands/creategamenight.js
+++ ibotv1-master/commands/creategamenight.js
@@ -12,7 +12,7 @@ exports.run = async (Discord, client, message, args) => {
     let host = message.author;
     let link = args [3];
     if (!mutetime) return message.reply("You didn't specify a time! Please specify how many ms from now you would like the gamenight!");
-    await (staffc.send("**A gamenight has been scheduled!**\n Game: " + game + "\n Host: " + host + "\n Time From Now: " + ms(mutetime) + "ms"));
+    await (staffc.send("**A gamenight has been scheduled!**\n \nGame: " + game + "\n Host: " + host + "\n Time From Now: " + ms(mutetime) + "ms"));
     message.reply("Gamenight has been created for: " + ms(mutetime) + "ms from now!");
     let announcetwo = mutetime-600000
     setTimeout(function() {
