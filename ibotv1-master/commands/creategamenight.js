46d454f6a10c34c5f8da3b8cd5c322b2cb381102
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-05-22 13:00:51 +0100

    Update creategamenight.js

--- ibotv1-master/commands/creategamenight.js
+++ ibotv1-master/commands/creategamenight.js
@@ -3,24 +3,21 @@ const ms = require("ms");
 
 exports.run = async (Discord, client, message, args) => {
     let staffc = message.guild.channels.find("name", "gamenight")
-    if (!staffc) return message.channel.send("I can not find the log channel. For logs, get the sever admin to make a channel called `logs`.");
+    if (!staffc) return message.channel.send("I can not find the gamenight channel!");
     if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Sorry, but you do not have valid permissions! If you beleive this is a error, contact an owner.");
     
 
     let mutetime = args[1];
     if (!mutetime) return message.reply("You didn't specify a time! Please specify how many ms from now you would like the gamenight!");
 
-    await (tomute.addRole(muterole.id));
-    message.reply(`Gamenight has been created forfor ${ms(ms(mutetime))} from now!`);
-      const embed = new Discord.RichEmbed()
-            .setColor(0x8cff00)
-            .setTimestamp()
-            .setDescription(`**Action:** Mute\n**Target:** ${tomute.tag}\n**Moderator:** ${message.author.tag}\n**Length:** ${ms(ms(mutetime))}`);
-        staffc.send({embed});
-        message.tomute.send(`You have been muted for **${ms(ms(mutetime))}** by ${message.author.tag}`);
+    await (staffc.send("A gamenight has been scheduled!\n Game:" + game"\n Time From Now:" + "ms(mutetime));
+    message.reply("Gamenight has been created for" + ms(mutetime) + "from now!");
+    let announcetwo = mutetime/2
+    setTimeout(function() {
+        message.channel.send(`<@${tomute.id}> has been unmuted!`);
+    }, ms(announcetwo));
     
     setTimeout(function() {
-        tomute.removeRole(muterole.id);
         message.channel.send(`<@${tomute.id}> has been unmuted!`);
     }, ms(mutetime));
 
