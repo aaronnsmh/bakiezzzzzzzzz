8c355c4619615d265fd7c4bfc054f6664f73fe5e
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-05-22 16:57:27 +0100

    Update hannounce.js

--- ibotv1-master/commands/hannounce.js
+++ ibotv1-master/commands/hannounce.js
@@ -4,14 +4,15 @@ exports.run = (Discord, client, message, args) => {
 		   let channel1 = message.mentions.channels.first()
 		   const color = args[0]
 		   let title = args[1];
+		   if (title = "mod") let icon = "https://media.discordapp.net/attachments/448221913978896387/448514229780086785/mod.png"
 		   const text = args.slice(2).join(" ");
 		   const author = message.author;
 		   if (text.length < 1) return message.channel.send("Can not announce nothing");
 		   //const colour = args.slice(2).join("");
 		   const embed = new Discord.RichEmbed()
 		   .setColor("#00ff00")
-		   .setThumbnail(client.user.avatarURL)
-		   .setTitle(title + "Announcement: ")
+		   .setThumbnail(icon)
+		   .setTitle(":mega: Announcement: ")
 		   .setDescription("**Announced by: " + message.author + "**\n\n" + text + "\n")
 		   .setFooter("An announcment made at ")
 		   .setTimestamp()
