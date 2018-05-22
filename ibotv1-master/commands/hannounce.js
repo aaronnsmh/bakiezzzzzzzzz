4780bd5fe96b9a039e641b3c9072eff7c4c60000
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-05-22 17:13:37 +0100

    Update hannounce.js

--- ibotv1-master/commands/hannounce.js
+++ ibotv1-master/commands/hannounce.js
@@ -6,6 +6,16 @@ exports.run = (Discord, client, message, args) => {
 		   let title = args[1];
 		   if (title == "mod"){
 			var icon = "https://media.discordapp.net/attachments/448221913978896387/448514229780086785/mod.png";
+		   } else if (title == "corp"){
+			var icon = "https://media.discordapp.net/attachments/448221913978896387/448515837394026497/corporate.png";
+		   } else if (title == "manag"){
+			var icon = "https://media.discordapp.net/attachments/448221913978896387/448515839239389184/management.png";
+		   } else if (title == "operation"){
+			var icon = "https://media.discordapp.net/attachments/448221913978896387/448515840154009622/operations.png";
+		   } else if (title == "com"){
+			var icon = "https://media.discordapp.net/attachments/448221913978896387/448515835997454346/communications.png";
+		   } else{
+			var icon = "https://media.discordapp.net/attachments/448221913978896387/448515840606863370/reg.png";
 		   }
 		   const text = args.slice(2).join(" ");
 		   const author = message.author;
