e6be6e568f41045469a2ad0c239462ba92a02c23
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-05-22 19:16:42 +0100

    Update creategamenight.js

--- ibotv1-master/commands/creategamenight.js
+++ ibotv1-master/commands/creategamenight.js
@@ -13,15 +13,45 @@ exports.run = async (Discord, client, message, args) => {
     let host = message.author;
     let link = args [3];
     if (!mutetime) return message.reply("You didn't specify a time! Please specify how many ms from now you would like the gamenight!");
-    await (staffc.send("**A gamenight has been scheduled!**\n \nGame: " + game + "\n Host: " + host + "\n Time From Now: " + ms(mutetime) + "ms"));
-    message.reply("Gamenight has been created for: " + ms(mutetime) + "ms from now!");
+
+    var emebedannounce = new Discord.RichEmbed()
+        
+    .setFooter("Bakiez Gamenight | Copyright 2018")
+    .setTitle("Bakiez Gamenight Announcement")
+    .setThumbnail(media.discordapp.net/attachments/448221913978896387/448515840606863370/reg.png)
+    .setDescription("Bakiez is having a gamenight! Check back here before it starts for a link!")
+    .addField("Game:", game)
+    .addField("Time (from now):", ms(mutetime))
+    .addField("Host:", host)
+
+    staffc.send("**A gamenight has been scheduled!**\n \nGame: " + game + "\n Host: " + host + "\n Time From Now: " + ms(mutetime) + "ms"));
+    message.reply("Gamenight has been created for: " + ms(mutetime) + "ms from now! Announcement should be pushed out.");
     let announcetwo = mutetime-300000
     setTimeout(function() {
-        message.channel.send(`@here\n A gamenight is starting in 5 mins! Details:\n **Game:** ` + game + "\n Link: " + link + "\n Host:" + host);
+        var emebedbefore = new Discord.RichEmbed()
+        
+.setFooter("Bakiez Gamenight | Copyright 2018")
+.setTitle("Bakiez Gamenight Announcement")
+.setThumbnail(media.discordapp.net/attachments/448221913978896387/448515840606863370/reg.png)
+.setDescription("The gamenight is due to be hosted in 5 minutes! Keep an eye out!")
+.addField("Host:", host)
+.addField("Game:", game)
+.addField("Link:" link)
+
+        message.channel.send(emebedbefore);
     }, ms(announcetwo));
     
     setTimeout(function() {
-        message.channel.send(`The gamenight is now starting! Join here: ` + link);
+        var emebedstart = new Discord.RichEmbed()
+        
+.setFooter("Bakiez Gamenight | Copyright 2018")
+.setTitle("Bakiez Gamenight Announcement")
+.setThumbnail(media.discordapp.net/attachments/448221913978896387/448515840606863370/reg.png)
+.setDescription("The gamenight is now starting!")
+.addField("Join Here:", link)
+
+message.channel.send(emebedstart);
+        
     }, ms(mutetime));
 
 }
