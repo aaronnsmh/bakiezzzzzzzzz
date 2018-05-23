65d541c636af24290dc38e80582f68468748d1a8
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-05-23 16:59:02 +0100

    Update help.js

--- ibotv1-master/commands/help.js
+++ ibotv1-master/commands/help.js
@@ -1,11 +1,18 @@
 var randomColor = Math.floor(Math.random() * 16777215).toString(16);
 const db = require('quick.db')
 exports.run = (Discord, client, message, args) => {
-    
-    let thumbnail = client.user.avatarURL;
-    let name = client.user.username;
 
-        message.reply("http://ibot.space/, the prefix in this server is '!'");
+
+        const embed = new Discord.RichEmbed()
+		   .setColor("#a9d9db")
+		   .setTitle("Bakiez Bot Commands:")
+		   .setDescription("Below are the commands for Bakiez Bot. NOTE: These can only be used by users with the `[-] Perm` discord role.")
+           .addField("Announce", "Announce #channel Department Description")
+           .addField("Say", "Say Message")
+           .addField("CreateGamenight", "Cg #channel Time(unit) Game Link")
+		   .setFooter("Want your own custom bot? Head to ibot.space or contact TheReal#1781!")
+		   .setTimestamp()
+message.channel.send({embed})
         
 
 }
