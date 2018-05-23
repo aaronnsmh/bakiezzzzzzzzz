5f780d0c1fbecdc648d5883cd5101177e8a57794
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-05-23 17:42:09 +0100

    Update hannounce.js

--- ibotv1-master/commands/hannounce.js
+++ ibotv1-master/commands/hannounce.js
@@ -6,25 +6,29 @@ exports.run = (Discord, client, message, args) => {
 		   if (title == "mod"){
 			var icon = "https://media.discordapp.net/attachments/448221913978896387/448514229780086785/mod.png";
 			var color = "#857574";
+			   var text = args.slice(2).join(" ");
 		   } else if (title == "corp"){
 			var icon = "https://media.discordapp.net/attachments/448221913978896387/448515837394026497/corporate.png";
 			var color = "#b8dee0";
+			   var text = args.slice(2).join(" ");
 		   } else if (title == "manag"){
 			var icon = "https://media.discordapp.net/attachments/448221913978896387/448515839239389184/management.png";
 			var color = "#c59989";
+			   var text = args.slice(2).join(" ");
 		   } else if (title == "operation"){
 			var icon = "https://media.discordapp.net/attachments/448221913978896387/448515840154009622/operations.png";
 			var color = "#c1b6b4";
+			   var text = args.slice(2).join(" ");
 		   } else if (title == "com"){
 			var icon = "https://media.discordapp.net/attachments/448221913978896387/448515835997454346/communications.png";
 			var color = "#887776";
+			   var text = args.slice(2).join(" ");
 		   } else{
 			var icon = "https://media.discordapp.net/attachments/448221913978896387/448515840606863370/reg.png";
 			var color = "6d6e6f";
+			var text = args.slice(1).join(" ");
 		   }
-		   const text = args.slice(2).join(" ");
 		   const author = message.author;
-		   if (text.length < 1) return message.channel.send("Can not announce nothing");
 		   //const colour = args.slice(2).join("");
 		   const embed = new Discord.RichEmbed()
 		   .setColor(color)
