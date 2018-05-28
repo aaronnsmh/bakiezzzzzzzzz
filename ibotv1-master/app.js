0f0a82beecc029296f443054361498e9dd8f27fb
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-05-28 21:42:40 +0100

    Update app.js

--- ibotv1-master/app.js
+++ ibotv1-master/app.js
@@ -43,7 +43,7 @@ client.on('message', message => {
    var guildid = message.guild.id
     db.fetch(`guildPrefix_${guildid}`).then(i => {
 
-        let prefix = i || '!'
+        let prefix = i || '='
 
         let msg = message.content.toLowerCase();
         let args = message.content.slice(prefix.length).trim().split(" ");
