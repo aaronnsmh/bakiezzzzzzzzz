e82dcd2cf6904c96e051d05d0996bb26e4be4084
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-06-19 17:17:26 +0100

    Update sayembed.js

--- ibotv1-master/commands/sayembed.js
+++ ibotv1-master/commands/sayembed.js
@@ -2,7 +2,7 @@ exports.run = (Discord, client, message, args) => {
 if (!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send("You must be an `ADMINISTRATOR` to use the `sayembed` command in the " + message.guild.name + " server!")
 let message2 = args.slice(0).join(" ");
 const embed = new Discord.RichEmbed()
-            .setColor("#6d6e6f")
+            .setColor("#a9d9db")
             .setDescription(message2);
         message.channel.send({embed});
 }
