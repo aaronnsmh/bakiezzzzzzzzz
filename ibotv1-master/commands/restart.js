64dd0572d24f3f5122cb642b37eeb2967421f968
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-07-15 11:47:32 +0100

    Update restart.js

--- ibotv1-master/commands/restart.js
+++ ibotv1-master/commands/restart.js
@@ -1,8 +1,6 @@
 exports.run = async (Discord, client, message, args) => {
     if (message.author.id !== '178350684922576896' && message.author.id !== '281060171730649089' && message.author.id !== '272712813934411796') return message.channel.send("You have invalid permissions");
-    await client.user.setStatus("online");
-    await client.user.setActivity('Restarting', { type: 'PLAYING' });
-    message.channel.send('Restarting, please wait for the bot status to go back to green before running next command.')
+    message.channel.send('Restarting, please wait 30 seconds before running next command.')
     setInterval(function(){ process.exit(); }, 3000);
       
     
