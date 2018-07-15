072e1f53d9b59cf1b4fb30952e21fc7dc0ce77ab
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-07-15 11:41:50 +0100

    Update restart.js

--- ibotv1-master/commands/restart.js
+++ ibotv1-master/commands/restart.js
@@ -1,7 +1,7 @@
 exports.run = (Discord, client, message, args) => {
-    if (message.author.id !== '178350684922576896' && message.author.id !== '281060171730649089' && message.author.id !== '272712813934411796') return message.channel.send("You have invalid permissions");
       message.channel.send('Restarted!');
-      
+      client.user.setStatus("idle");
+      client.user.setActivity('Restarting', { type: 'PLAYING' });
       setInterval(function(){ process.exit(); }, 3000);
       
     
